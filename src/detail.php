c0c9708b317decc9ed7a23f3b7ecb696c99349aa
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-11 12:34:52 +0900

    a

---
+++ src/detail.php
@@ -0,0 +1,8 @@
+<?php require 'db-connect.php';?>
+
+foreach ($sql as $row){
+echo '<p><img alt="image" src="image/', $row['id'], '.jpg"></p>';
+echo '<form action="index.php" method="post">';
+echo '<p>番号:', $row['drama_id'], '</p>';
+echo '<p>作品名:', $row['name'], '</p>';
+echo '<p>年:', $row['year'], '</p>';
