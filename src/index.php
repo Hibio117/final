2ab675ea9f4baf6d92d412f8b187e7eac7b7616e
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-11 12:11:14 +0900

    a

--- src/index.php
+++ src/index.php
@@ -1,15 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<?php require 'db-connect.php';?>
+
     <h1>韓国ドラマ管理メニュー</h1>
     <input type="submit" value="一覧">
     <input type="submit" value="登録">
     <input type="submit" value="更新">
     <input type="submit" value="削除">
-</body>
-</html>
