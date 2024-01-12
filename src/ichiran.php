be1ecd1c8a999d5cf7fe0585a3a050a3df190e2e
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-12 12:42:47 +0900

    a

--- src/ichiran.php
+++ src/ichiran.php
@@ -1,22 +1,34 @@
 <?php require 'db-connect.php';?>
 
 
-    韓国ドラマ一覧
-    <a href="index.php" >トップに戻る</a>
-<hr>
-<h1>作品一覧</h1>
-    <table>
-        <tr><th>番号</th><th>作品名</th><th>年</th></tr>
-    <?php
-    $pdo=new PDO($connect, USER, PASS);
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>作品一覧</title>
+</head>
+<body>
+    <div id="content">
+    <h1>作品一覧</h1>
 
+    <table border="1">
+        <tr>
+            <th>　作品ID　</th><th>　作品名　</th><th>　年　</th>
+        </tr>
+
+<?php
+    $pdo=new PDO($connect, USER, PASS);
     foreach($pdo->query('select * from drama') as $row){
         echo '<tr>';
         echo '<td>',$row['drama_id'],'</td>';
         echo '<td>',$row['name'],'</td>';
         echo '<td>',$row['year'],'</td>';
-        echo '</tr>';
+        echo '<td>';
+        echo '</td>';
         echo "\n";
      }
 ?>
-    </table>
+</table>    
+</body>
+</html>
