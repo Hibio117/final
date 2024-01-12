4726cd0ab563409e005bafbb7e9cafcd439df40b
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-12 11:30:00 +0900

    a

--- src/index.php
+++ src/index.php
@@ -1,6 +1,8 @@
 <?php require 'db-connect.php';?>
 
     <h1>韓国ドラマ管理メニュー</h1>
+
+    <form action="ichiran.php" method="post">
     <input type="submit" value="一覧">
     <input type="submit" value="登録">
     <input type="submit" value="更新">
