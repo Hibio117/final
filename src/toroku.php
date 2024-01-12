abb8ca8213a781f5bec4a9d71d4ff04b7ebb23fa
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-12 11:59:17 +0900

    a

--- src/toroku.php
+++ src/toroku.php
@@ -2,7 +2,6 @@
 
 作品を登録
 
-<form action="kadai1-2-output.php" method="post">
     作品名：<input type="text" name="name"><br>
     年：<input type="text" name="year"><br>
     <button type="submit">登録</button>
