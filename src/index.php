105190f9d531c5a9e2befc87cc6164d64da7d8e8
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-12 12:13:02 +0900

    a

--- src/index.php
+++ src/index.php
@@ -3,13 +3,13 @@
     <h1>韓国ドラマ管理メニュー</h1>
 
     <form action="ichiran.php" method="post">
-    <input type="submit" value="一覧">
+    <input type="submit" value="一覧"></form>
 
     <form action="toroku.php" method="post">
-    <input type="submit" value="登録">
+    <input type="submit" value="登録"></form>
 
     <form action="update.php" method="post">
-    <input type="submit" value="更新">
+    <input type="submit" value="更新"></form>
 
     <form action="delete.php" method="post">
-    <input type="submit" value="削除">
+    <input type="submit" value="削除"></form>
