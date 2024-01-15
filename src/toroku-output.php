ff677e6eeefae7c7a6fa7ca05135bb3ccb2913cc
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-15 11:47:09 +0900

    a

--- src/toroku-output.php
+++ src/toroku-output.php
@@ -10,7 +10,7 @@
     $pdo=new PDO($connect,USER,PASS);
     $sql=$pdo->prepare('insert into drama(name,year)values(?,?)');
     if(empty($_POST['name'])){
-        echo'商品名を入力してください。';
+        echo'作品名を入力してください。';
     }else if($sql->execute([$_POST['name'],$_POST['year']]){
         echo '<font color="red">追加しました。</font>';
     }else{
@@ -20,24 +20,19 @@
     <br><hr><br>
 
 <table border="1">
-    <tr><th>　商品ID　</th><th>　商品名　</th><th>　説明文　</th><th>　単価　</th><th>　画像パス　</th><th>　登録日　</th><th>　更新日　</th></tr>
+    <tr><th>　作品ID　</th><th>　年　</th></tr>
 
 <?php
-foreach($pdo->query('select * from LipLip') as $row){
+foreach($pdo->query('select * from drama') as $row){
     echo'<tr>';
-    echo'<td>',$row['shohin_id'],'</td>';
-    echo'<td>',$row['shohin_mei'],'</td>';
-    echo'<td>',$row['explanation'],'</td>';
-    echo'<td>',$row['shohin_tanka'],'</td>';
-    echo'<td>',$row['img_pass'],'</td>';
-    echo'<td>',$row['torokubi'],'</td>';
-    echo'<td>',$row['kosinbi'],'</td>';
+    echo'<td>',$row['drama_id'],'</td>';
+    echo'<td>',$row['year'],'</td>';
     echo'</tr>';
     echo"\n";
 }
 ?>
 </table>
     <br>
-    <button onclick="location.href='insert-input.php'">追加画面へ戻る</button>
+    <button onclick="location.href='toroku-input.php'">追加画面へ戻る</button>
 </body>
 </html>
