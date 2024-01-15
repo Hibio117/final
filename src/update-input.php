8d5c853ad8d01d94657fd720f9eb649f59234b60
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-15 19:49:13 +0900

    a

---
+++ src/update-input.php
@@ -0,0 +1,35 @@
+<?php require 'db-connect.php';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="th0">作品ID</div>
+    <div class="th1">作品名</div>
+    <div class="th1">年</div>
+
+<?php
+    $pdo=new PDO($connect, USER, PASS);
+
+    foreach ($pdo->query('select * from drama') as $row) {
+        echo '<form action="update-input.php" method="post">';
+        echo '<input type="hidden" name="id" value="', $row['drama_id'], '">';
+        echo '<div class="td0">',$row['drama_id'],'</div>';
+        echo '<div class="td1">';
+        echo '<input type="text" name="name" value="', $row['name'], '">';
+        echo '</div> ';
+        echo '<div class="td1">';
+        echo ' <input type="text" name="year" value="', $row['year'],'">';
+        echo '</div> ';
+        echo '<div class="td2"><input type="submit" value="更新"></div>';
+        echo '</form>';
+        echo "\n";
+    }
+    
+?>
+</body>
+</html>
---
+++ src/update-output.php
@@ -0,0 +1,50 @@
+<?php require 'db-connect.php';?><?php require 'db-connect.php';?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $pdo=new PDO($connect, USER, PASS);
+
+    $sql=$pdo->prepare('update drama set name=? where id=?');
+
+    if (empty($_POST['drama_id'])) {
+        echo '作品IDを入力してください。';
+    } else
+    if (empty($_POST['name'])){
+        echo '作品名を入力してください。';
+    } else
+    if (empty($_POST['year'])){
+        echo '年を入力してください。';
+    }else
+
+    if ($sql->execute([htmlspecialchars($_POST['drama_id']), $_POST['name'], $_POST['year']])){
+        echo '更新しました。';
+    }else{
+        echo '更新できませんでした。';
+    }
+?>
+    <hr>
+    <table>
+        <tr><th>作品ID</th><th>作品名</th><th>年</th></tr>
+    
+    <?php
+    foreach ($pdo->query('select * from drama') as $row){
+        echo '<tr>';
+        echo '<td>', $row['drama_id'],'</td>';
+        echo '<td>',$row['name'],'</td>';
+        echo '<td>',$row['year'],'</td>';
+        echo '</tr>';
+        echo "\n";
+    }
+    ?>
+        </table>
+        <button onclick=location.href='update-input.php'">更新画面へ戻る</button>
+</body>
+</html>
