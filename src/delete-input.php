d3c34b799523c35858ccd41a026758c696d881ef
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-16 00:17:41 +0900

    a

---
+++ src/delete-input.php
@@ -0,0 +1,30 @@
+<?php require 'db-connect.php';?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table>
+<tr><th>作品番号</th><th>作品名</th><th>年</th>
+<?php
+    $pdo=new PDO($connect, USER, PASS);
+    foreach ($pdo->query('select * from drama') as $row){
+        echo '<tr>';
+        echo '<td>',$row['drama_id'],'</td>';
+        echo '<td>',$row['name'],'</td>';
+        echo '<td>',$row['year'],'</td>';
+        echo '<td>';
+        echo'<a href="delete-output.php?id=', $row['drama_id'], '">削除</a>';
+        echo '</td>';
+        echo '</th>';
+        echo "\n";
+    }
+?>
+    </table>
+</body>
+</html>
---
+++ src/delete-output.php
@@ -0,0 +1,39 @@
+<?php require 'db-connect.php';?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $pdo=new PDO($connect, USER, PASS);
+        $sql=$pdo->prepare('delete from drama where id=?');
+        if($sql->execute([$_GET['drama_id']])){
+            echo '削除しました。';
+        }else{
+            echo '削除できませんでした。';
+        }
+    ?>
+        <br><hr><br>
+        <table>
+            <tr><th>作品番号</th><th>作品名</th><th>年</th></tr>
+
+        <?php
+            foreach ($pdo->query('select * from drama') as $row){
+                echo '<tr>';
+                echo '<td>',$row['drama_id'],'</td>';
+                echo '<td>',$row['name'],'</td>';
+                echo '<td>',$row['year'],'</td>';
+                echo '</tr>';
+                echo "\n";
+            }
+        ?>
+        </table>
+            <form action="delete-input.php" method="post">
+                <button type="submit">削除画面へ戻る</button>
+        </form>
+</body>
+</html>
