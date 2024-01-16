2c261c7ab2d0deaa2216deed126f97d1240d4e80
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-16 23:13:55 +0900

    a

--- src/update-output.php
+++ src/update-output.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/update-input.css">
+    <link rel="stylesheet" href="css/update-output.css">
     <title>Document</title>
 </head>
 <body>
