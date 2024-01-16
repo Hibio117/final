e94154476c19dce7516577dbcfa9a3c64987c314
Hibio117 <2201181@s.asojuku.ac.jp>
2024-01-17 00:36:11 +0900

    a

--- src/ichiran.php
+++ src/ichiran.php
@@ -13,6 +13,9 @@
     <div id="content">
     <h1>作品一覧</h1>
     <button class="modoru" onclick="location.href='index.php'">トップに戻る</button>
+
+    <a href="" class="btn btn--pink btn--raised btn--cubic">PUSH！<i class="fas fa-angle-right fa-position-right"></i></a>
+
     <br>
     <br>
     <br>
